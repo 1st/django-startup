ca0064a0a1b99d8903e74893b283515bd5a28ad6
Anton Danilchenko <anton.danilchenko@comodo.od.ua>
2013-03-31 23:26:49 +0300

    FIX: fixed problem with load static files

--- new_project/apps/project/settings.py
+++ new_project/apps/project/settings.py
@@ -89,7 +89,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -100,6 +100,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    'static',
 )
 
 # List of finder classes that know how to find static files in
