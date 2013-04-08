54edf50a89517c578626cb4489a464a4f8f03a69
Anton Danilchenko <anton.danilchenko@comodo.od.ua>
2013-04-08 15:55:18 +0300

    FIX: replaced name to namespace for urls() that we include

--- new_project/apps/project/urls.py
+++ new_project/apps/project/urls.py
@@ -6,6 +6,11 @@ When you crate new application, you need to do:
  - create copy of last "url(...)" statement
  - replace "new_app" to real name of your application.
  - uncomment this line (delete initial "#" symbol)
+
+To reverse url use syntax:
+ - in python code "from django.core.urlresolvers import reverse"
+   and "url = reverse('action_name', current_app='new_app')"
+ - in templates "{% url new_app:action_name %}"
 '''
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
@@ -18,5 +23,5 @@ urlpatterns = patterns('',
     url(r'^$', 'apps.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
     # copy this line for each new application that you have
-    # url(r'^new_app/', include('apps.new_app.urls'), name='new_app'),
+    # url(r'^new_app/', include('apps.new_app.urls'), namespace='new_app'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
